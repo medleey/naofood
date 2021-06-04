9e7d4d6708f3eed1c750a7db4ca43fc3332e3125
Alice <coisne.alice@gmail.com>
2021-06-04 11:50:03 +0200

    edit home

--- front/src/components/Home.vue
+++ front/src/components/Home.vue
@@ -1,13 +1,20 @@
 <template>
   <div> 
     <img src="/img/burger.jpeg">
+    
     <div class="container mx-auto text-center titles absolute">
       <h1 class="font-bold text-4xl text-white">Naofood</h1>
       <p class="bg-secondary p-4 text-white baseline">La nouvelle livraison éthique</p>
     </div>
-    <section class="bg-primary p-4 flex flex-wrap justify-center items-center hot my-4 container mx-auto">
+
+    
+    <section class="bg-primary p-4 flex flex-wrap justify-center items-center w-full">
+      
       <div class="mr-4 space-y-2">
-        <h2 class="text-secondary font-bold">ZONE DE LIVRAISON - 2,5KM DEPUIS LE CENTRE</h2>
+        <div class="flex items-center">
+          <i class="fas fa-biking text-secondary mr-2 text-2xl"></i>
+          <h2 class="text-secondary font-bold">ZONE DE LIVRAISON - 2,5KM DEPUIS LE CENTRE</h2>
+        </div>
         <p class="text-white">2,5km à vélo, c'est l'assurance d'un repas qui arrive CHAUD !</p>
       </div>
       <img src="/img/map.jpeg" class="map mt-4 md:mt-0">    
@@ -45,11 +52,8 @@ export default {
   .titles {
     top: 200px
   }
-  .hot{
-    border-radius: 20px;
-    max-width: 700px
-  }
   .map{
     width: 200px;
   }
+
 </style>
