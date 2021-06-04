3b4a7600c5d7d101774435de833aae048c0edee6
Robin Couet <couetrobin@gmail.com>
2021-06-04 14:30:07 +0200

    card grid

--- front/src/components/New.vue
+++ front/src/components/New.vue
@@ -1,41 +1,23 @@
 <template>
-  <div>
-    <section class="container mx-auto m-8">
-        <h2 class="font-bold text-xl text-secondary">LES NOUVEAUTÉS</h2>
-        <p class="mb-4">Ils rejoignent l'aventure !</p>
-        <div class="flex flex-wrap">
-          <div class="bg-gray-300 p-8 bg-card w-full">
-              <div class="card bg-white p-4 text-center">
-                  <img src="img/burger.jpeg">
-                  <div class="content">
-                    <h3 class="font-bold text-secondary text-lg mt-2">Nom restaurant</h3>
-                    
-                    <h4>TYPE DE RESTAURANT</h4>
-                    <p class="mt-2 mb-4 text-sm">
-                        Une rôtisserie dans un esprit streetfood qui vous propose des recettes savoureuses inspirées de la cuisine portugaise et espagnole
-                    </p>
-                    <a class="bg-primary text-white text-sm p-2 btn hover:bg-secondary hover:text-primary my-2">Voir la carte</a>
-                  </div>
-              </div>
-              <div class="bg-gray-300 p-8 bg-card">
-                <div class="card bg-white p-4 text-center">
-                    <img src="img/burger.jpeg">
-                    <div class="content">
-                      <h3 class="font-bold text-secondary text-lg mt-2">Nom restaurant</h3>
-                      
-                      <h4>TYPE DE RESTAURANT</h4>
-                      <p class="mt-2 mb-4 text-sm">
-                          Une rôtisserie dans un esprit streetfood qui vous propose des recettes savoureuses inspirées de la cuisine portugaise et espagnole
-                      </p>
-                      <a class="bg-primary text-white text-sm p-2 btn hover:bg-secondary hover:text-primary my-2">Voir la carte</a>
-                    </div>
-                </div>
-                <a class="bg-secondary text-white text-sm p-2 btn hover:bg-primary hover:text-white my-2">Voir la carte</a>
-            </div>
+  <section class="container mx-auto m-8">
+      <h2 class="font-bold text-xl text-secondary">LES NOUVEAUTÉS</h2>
+      <p class="mb-4">Ils rejoignent l'aventure !</p>
+      <div class="grid md:grid-cols-2 lg:grid-cols-3 gap-8 bg-gray-300 p-8 rounded-xl">
+        <div class="bg-white text-center w-full rounded-xl">
+          <img src="img/burger.jpeg" class="w-full h-36 object-cover rounded-t-xl max-w-xs ml-auto">
+          <img src="img/burger.jpeg" class="w-full h-36 object-cover rounded-t-xl max-w-xs ml-auto">
+          <div class="content p-4">
+            <h3 class="font-bold text-secondary text-lg mt-2">Nom restaurant</h3>
+            <h4>TYPE DE RESTAURANT</h4>
+            <p class="mt-2 mb-4 text-sm">
+                Une rôtisserie dans un esprit streetfood qui vous propose des recettes savoureuses inspirées de la cuisine portugaise et espagnole
+            </p>
+            <a class="bg-primary text-white text-sm p-2 btn hover:bg-secondary hover:text-primary my-2">Voir la carte</a>
           </div>
         </div>
-    </section>
-  </div>
+      </div>
+      <a class="bg-secondary text-white text-sm p-2 btn hover:bg-primary hover:text-white my-2">Voir la carte</a>
+  </section>
   
 </template>
 
@@ -58,12 +40,6 @@ export default {
   }
   .bg-card{
     border-radius: 20px; 
-  }
-  img{
-    height: 150px;
-    max-width: 250px;
-    width: 100%;
-    border-top-right-radius: 20px;
   }
   .btn{
     border-radius: 10px;
